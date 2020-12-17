67795818ada79b2e655f1331669a89ae6c172ff8
unknown <victor.hugo.pacheco.flores@gmail.com>
2020-12-17 13:36:19 -0600

    Remove the puts in the class and adding it in the main.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -2,4 +2,14 @@ require './lib/rubo'
 
 puts "Hi, this rubocop takes the directory 'examples' and work on all files inside on it, let's start..."
 directory = Dir['./examples/**/*.rb']
-Rubo.run(directory)
+directory.each do |i|
+  script = File.open(i)
+  script.each_with_index do |line, j|
+    print Rubo.right_spaces(i, line, j)
+    print Rubo.blank_line(i, line, j)
+    print Rubo.semicolon(i, line, j)
+    print Rubo.nav_operator(i, line, j)
+    print Rubo.space_between_elements(i, line, j)
+    print Rubo.exponent(i, line, j)
+  end
+end
--- lib/rubo.rb
+++ lib/rubo.rb
@@ -1,42 +1,28 @@
 class Rubo
-  def self.run(directory)
-    directory.each do |i|
-      script = File.open(i)
-      script.each_with_index do |line, j|
-        right_spaces(i, line, j)
-        blank_line(i, line, j)
-        semicolon(i, line, j)
-        nav_operator(i, line, j)
-        space_between_elements(i, line, j)
-        exponent(i, line, j)
-      end
-    end
-  end
-
   def self.right_spaces(name, line, num)
-    puts "#{name} line:#{num + 1} 'Right blank space(s) in the line.'" if line.match(/\s{2,}$/)
+    "#{name} line:#{num + 1} 'Right blank space(s) in the line.'\n" if line.match(/\s{2,}$/)
   end
 
   def self.blank_line(name, line, num)
-    puts "#{name} line:#{num + 1} 'Blank line at the start of the script'" if line.match(/^\n/)
+    "#{name} line:#{num + 1} 'Blank line at the start of the script'\n" if line.match(/^\n/)
   end
 
   def self.semicolon(name, line, num)
-    puts "#{name} line:#{num + 1} 'Semicolon is not neccesary'" if line.match(/;$/)
+    "#{name} line:#{num + 1} 'Semicolon is not neccesary'\n" if line.match(/;$/)
   end
 
   def self.nav_operator(name, line, num)
-    puts "#{name} line:#{num + 1} 'No space required between navigation operator'" if line.match(/\ &.|&.\ /)
+    "#{name} line:#{num + 1} 'No space required between navigation operator'\n" if line.match(/\ &.|&.\ /)
   end
 
   def self.space_between_elements(name, line, num)
     if line.match(/\[\w|\w\]/)
-      puts "#{name} line:#{num + 1} 'Space required between open/close keys and first/last element'"
+      "#{name} line:#{num + 1} 'Space required between open/close keys and first/last element'\n"
     end
-    puts "#{name} line:#{num + 1} 'Space required between separator and array element'" if line.match(/\[.*,\w.*\]/)
+    "#{name} line:#{num + 1} 'Space required between separator and array element'\n" if line.match(/\[.*,\w.*\]/)
   end
 
   def self.exponent(name, line, num)
-    puts "#{name} line:#{num + 1} 'No space required between an exponent operator'" if line.match(/\ \*\*|\*\*\ /)
+    "#{name} line:#{num + 1} 'No space required between an exponent operator'\n" if line.match(/\ \*\*|\*\*\ /)
   end
 end
